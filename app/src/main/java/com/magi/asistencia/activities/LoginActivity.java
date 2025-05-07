b4cffef6cb7f24f903c7c7de1cab226bc71ad375
Jota-FalseProphet <jotaperdomo2552@gmail.com>
2025-05-08 00:55:38 +0200

    se rompio el login

--- app/src/main/java/com/magi/asistencia/activities/LoginActivity.java
+++ app/src/main/java/com/magi/asistencia/activities/LoginActivity.java
@@ -21,7 +21,8 @@ import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private static final String BASE_URL = "https://159.69.215.108/api/login";
+    //private static final String BASE_URL = "https://159.69.215.108/api/login";
+    public static final String BASE_URL = "http://159.69.215.108:80/";
 
     //OLVIdÉ QUE VERSiÓN DE ANDROID ESTOY USANDO XDDDD
 
